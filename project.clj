a820170646264a4fb52ce83fb1d05240a49eba5b
MasterBlaster <r.gersak@gmail.com>
2016-01-12 21:55:57 +0100

    Minor change to project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject kovacnica/clojure.network.terminal "0.1.0"
   :description "Communicate with network/equipment with terminal. Supported protocols are
                SSH and Telnet."
-  :url ""
+  :url "https://github.com/kovacnica/clojure.network.terminal.git"
   :aot :all
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [prismatic/schema "1.0.4"]
                  [org.clojure/core.async "0.2.374"]
                  [ch.ethz.ganymed/ganymed-ssh2 "262"]
                  [kovacnica/clojure.network.ip "0.1.1"]
